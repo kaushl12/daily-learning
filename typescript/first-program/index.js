23413de38d8360d94446294b935a30c3a615e06b
kaushl12 <kaushalchaudhari82@.com>
2025-11-19 01:23:36 +0530

    First Typescript program

---
+++ typescript/first-program/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let x = 10;
+x = 20;
+console.log(x);
+//# sourceMappingURL=index.js.map
---
+++ typescript/first-program/index.ts
@@ -0,0 +1,3 @@
+let x: number = 10;
+ x=20;
+console.log(x);
