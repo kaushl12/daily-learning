862ee2c081617eb00e84532b57579661e4a126b9
kaushl12 <kaushalchaudhari82@.com>
2025-11-18 21:09:29 +0530

    made maincontent responsive

--- tailwind/sidebar-tailwind/src/components/Maincontent.jsx
+++ tailwind/sidebar-tailwind/src/components/Maincontent.jsx
@@ -1,28 +1,58 @@
-import React from 'react'
+import React from "react";
 
 function Maincontent() {
   return (
     <>
-        <div className=" w-full ">
+      <div className="w-full bg-gray-100">
+        <div className="h-32 bg-black"></div>
 
-
-            <div className='h-32 bg-black'></div>
-            <div className='grid grid-cols-11 gap-8 p-5 '>
-                <div className='h-72 rounded 2xl bg-white p-3 col-span-2  -translate-y-14 shadow-lg ml-2 flex flex-col justify-center items-center'>
-                    <img className="h-auto max-w-32 mb-3 " src="./src/assets/profile.jpg" alt="image description"></img>
-                    <div className='font-bold' >Kaushal CK</div>
-                    <div className='mt-2 text-gray-500 font-medium opacity-60' >kaushal@gmail.com</div>
-                    <div className='mt-1  text-gray-500  font-medium opacity-60'>9876453210</div>
-                    <div className='mt-3 mb-3  text-gray-500 font-medium opacity-60'>Delhi,India</div>
-
-                </div>
-                <div className='h-96 rounded 2xl bg-blue-200 col-span-6 shadow-lg'>Card-2</div>
-                <div className='h-96 rounded 2xl bg-red-200 col-span-3 shadow-lg'>Card-3</div>
-            </div>
+        <div className="px-5 -mt-10 md:mt-5">
+          <h1 className="text-3xl font-bold text-gray-800">Good Morning,</h1>
+          <p className="text-xl text-gray-600 font-medium">Prabhleen</p>
         </div>
 
+        <div className="grid grid-cols-11 gap-8 p-5">
+          <div
+            className="
+            h-72 rounded-2xl bg-white p-3 
+            col-span-11 md:col-span-2  
+            -translate-y-14 md:-translate-y-14
+            shadow-lg ml-2 md:ml-0
+            flex flex-col justify-center items-center
+          "
+          >
+            <img
+              className="h-auto max-w-32 mb-3 rounded-full"
+              src="./src/assets/profile.jpg"
+              alt="profile"
+            />
+            <div className="font-bold text-lg">Kaushal CK</div>
+            <div className="mt-2 text-gray-600">kaushal@gmail.com</div>
+            <div className="mt-1 text-gray-600">9876453210</div>
+            <div className="mt-3 text-gray-600">Delhi, India</div>
+          </div>
+
+          <div
+            className="
+            h-96 rounded-2xl bg-blue-200 shadow-lg 
+            col-span-11 md:col-span-6
+          "
+          >
+            Card-2
+          </div>
+
+          <div
+            className="
+            h-96 rounded-2xl bg-red-200 shadow-lg 
+            col-span-11 md:col-span-3
+          "
+          >
+            Card-3
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Maincontent
+export default Maincontent;
